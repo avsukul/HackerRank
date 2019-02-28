9807109390c193d85fe7dfca0521b33b1bba1acc
Victor <amarvsukul@live.ca>
2019-02-28 12:38:58 -0500

    updated merchant.

--- src/main/java/Hacker/rank/Merchant.java
+++ src/main/java/Hacker/rank/Merchant.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class Merchant {
 	
+    public static void main(String[] args) throws IOException {
+    	int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20};
+    	System.out.println(sockMerchant(9, ar));
+    }
+    
     // Complete the sockMerchant function below.
     static int sockMerchant(int n, int[] ar) {
     	int ind = 0;
@@ -24,36 +29,6 @@ public class Merchant {
     			continue;
     		}
     	}
-    	System.out.println(">>> " + cnt);
-    	return 0;
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-//        int n = scanner.nextInt();
-//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-//
-//        int[] ar = new int[n];
-//
-//        String[] arItems = scanner.nextLine().split(" ");
-//        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-//
-//        for (int i = 0; i < n; i++) {
-//            int arItem = Integer.parseInt(arItems[i]);
-//            ar[i] = arItem;
-//        }
-    	
-    	int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20};
-        int result = sockMerchant(9, ar);
-
-//        bufferedWriter.write(String.valueOf(result));
-//        bufferedWriter.newLine();
-//
-//        bufferedWriter.close();
-
-        scanner.close();
+    	return cnt;
     }
 }
