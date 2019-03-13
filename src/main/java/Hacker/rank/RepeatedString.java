4889daebb606a25b7d2a52ecb38654f1092fd4b0
Victor <amarvsukul@live.ca>
2019-03-13 18:16:18 -0400

    update.

--- src/main/java/Hacker/rank/RepeatedString.java
+++ src/main/java/Hacker/rank/RepeatedString.java
@@ -1,9 +1,9 @@
 package Hacker.rank;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 
 public class RepeatedString {
+
 
 	// Complete the repeatedString function below.
 	static long repeatedString(String s, long n) {
@@ -18,28 +18,103 @@ public class RepeatedString {
 				}
 			}
 		}
-		// 685118368975
-		long v = Math.round(n/s.length()/1.0);
-		System.out.println(v);
-		if ((n/s.length()) > 100.00) {
-			return Math.round(s.length()/1.0) * a + aa;
+		if ((n/s.length()) > 1000000.0) {
+			return (long) Math.round(Math.ceil((n/(s.length()*1.0)*1.00)/1.00 * a)) + aa;
 		}
 		return n/s.length() * a + aa;
 	}
 
 	public static void main(String[] args) throws IOException {
-//		String s = "aba"; long n = 10;
-//		System.out.println(repeatedString(s, n));
-		String s1 = "ojowrdcpavatfacuunxycyrmpbkvaxyrsgquwehhurnicgicmrpmgegftjszgvsgqavcrvdtsxlkxjpqtlnkjuyraknwxmnthfpt";
-		long n1 = 685118368975L;
-		System.out.println(repeatedString(s1, n1));
-//		String s2 = "abcacg"; long n2 = 10;
-//		System.out.println(repeatedString(s2, n2));
-//		String s3 = "abcac"; long n3 = 10;
-//		System.out.println(repeatedString(s3, n3));
-//		String s4 = "gfcaaaecbg"; long n4 = 547602;
-//		System.out.println(repeatedString(s4, n4));
-//		String s5 = "cfimaakj"; long n5 = 554045874191L;
-//		System.out.println(repeatedString(s5,(long) n5));
+		switch ("s10") {
+		case "s": 
+			String s = "aba"; long n = 10;
+			System.out.println(repeatedString(s, n));
+			break;
+
+		case "s1":
+			String s1 = "ojowrdcpavatfacuunxycyrmpbkvaxyrsgquwehhurnicgicmrpmgegftjszgvsgqavcrvdtsxlkxjpqtlnkjuyraknwxmnthfpt";
+			long n1 = 685118368975L;
+			System.out.println(repeatedString(s1, n1));
+			break;
+
+		case "s2":
+			String s2 = "abcacg"; long n2 = 10;
+			System.out.println(repeatedString(s2, n2));
+			break;
+
+		case "s3":
+			String s3 = "abcac"; long n3 = 10;
+			System.out.println(repeatedString(s3, n3));
+			break;
+
+		case "s4":
+			String s4 = "gfcaaaecbg"; long n4 = 547602;
+			System.out.println(repeatedString(s4, n4));
+			break;
+
+		case "s5":
+			String s5 = "cfimaakj"; long n5 = 554045874191L;
+			System.out.println(repeatedString(s5,(long) n5));
+			break;
+
+		case "s7":
+			String s7 = "a"; long n7 = 1000000000000L;
+			System.out.println(repeatedString(s7,(long) n7));
+			break;
+
+		case "s8":
+			String s8 = "aab"; long n8 = 882787;
+			System.out.println(repeatedString(s8, n8));
+			break;
+
+		case "s9":
+			String s9 = "epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq";
+			long n9 = 549382313570L;
+			System.out.println(repeatedString(s9, n9));
+			break;
+		
+		case "s10":
+			String s911 = "udjlitpopjhipmwgvggazhuzvcmzhulowmveqyktlakdufzcefrxufssqdslyfuiahtzjjdeaxqeiarcjpponoclynbtraaawrps";
+			long n911 = 872514961806L;
+			System.out.println(repeatedString(s911, n911));
+
+		default:
+			String s11 = "aba"; long n100 = 10;
+			System.out.println(repeatedString(s11, n100));
+
+			String s111 = "ojowrdcpavatfacuunxycyrmpbkvaxyrsgquwehhurnicgicmrpmgegftjszgvsgqavcrvdtsxlkxjpqtlnkjuyraknwxmnthfpt";
+			long n11 = 685118368975L;
+			System.out.println(repeatedString(s111, n11));
+
+			String s21 = "abcacg"; long n21 = 10;
+			System.out.println(repeatedString(s21, n21));
+
+			String s31 = "abcac"; long n31 = 10;
+			System.out.println(repeatedString(s31, n31));
+
+			String s41 = "gfcaaaecbg"; long n41 = 547602;
+			System.out.println(repeatedString(s41, n41));
+
+			String s51 = "cfimaakj"; long n51 = 554045874191L;
+			System.out.println(repeatedString(s51,(long) n51));
+
+			String s71 = "a"; long n71 = 1000000000000L;
+			System.out.println(repeatedString(s71,(long) n71));
+
+			String s81 = "aab"; long n81 = 882787;
+			System.out.println(repeatedString(s81, n81));
+
+			String s91 = "epsxyyflvrrrxzvnoenvpegvuonodjoxfwdmcvwctmekpsnamchznsoxaklzjgrqruyzavshfbmuhdwwmpbkwcuomqhiyvuztwvq";
+			long n91 = 549382313570L;
+			System.out.println(repeatedString(s91, n91));
+			
+			String s9111 = "udjlitpopjhipmwgvggazhuzvcmzhulowmveqyktlakdufzcefrxufssqdslyfuiahtzjjdeaxqeiarcjpponoclynbtraaawrps";
+			long n9111 = 872514961806L;
+			System.out.println(repeatedString(s9111, n9111));
+			
+			break;
+		}
 	}
 }
+
+
---
+++ src/main/java/Hacker/rank/t.java
@@ -0,0 +1,10 @@
+package Hacker.rank;
+
+public class t {
+	
+	public static void main(String[] args) {
+		System.out.println(Math.round(Math.pow((685118368975L/(100*1.0)*100.00)/100.00 * 6, 1)));
+		System.out.println(Math.round(Math.ceil((549382313570L / (100 * 1.0) * 100.00) / 100.00 * 3)));
+		System.out.println(Math.round(Math.ceil((872514961806L/(100*1.0)*100.00)/100.00 * 8)));
+	}
+}
